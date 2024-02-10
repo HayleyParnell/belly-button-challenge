7f56ecdde919ea49987283f1b4030a1b11993dc2
HayleyParnell <Hayley.Parnell@outlook.com>
2024-02-10 11:17:11 -0600

    update

--- Main Code/static/js/app.js
+++ Main Code/static/js/app.js
@@ -4,7 +4,7 @@ d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1
   // initialize the dashboard
   function init() {
     //dropdown menu 
-    var dropdownMenu = d3.select("#selDataset");
+    let dropdownMenu = d3.select("#selDataset");
 
     data.names.forEach(function(sample) {
       dropdownMenu.append("option").text(sample).property("value", sample);
@@ -18,12 +18,12 @@ d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1
   //build charts
   function buildCharts(sample) {
     // Filter data for the selected sample
-    var selectedSample = data.samples.filter(function(d) {
+    let selectedSample = data.samples.filter(function(d) {
       return d.id === sample;
     })[0];
 
     // Bar chart
-    var barData = [{
+    let barData = [{
       x: selectedSample.sample_values.slice(0, 10).reverse(),
       y: selectedSample.otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse(),
       text: selectedSample.otu_labels.slice(0, 10).reverse(),
@@ -31,7 +31,7 @@ d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1
       orientation: "h"
     }];
 
-    var barLayout = {
+    let barLayout = {
       title: "Top 10 OTUs",
       xaxis: { title: "Sample Values" }
     };
@@ -39,7 +39,7 @@ d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1
     Plotly.newPlot("bar", barData, barLayout);
 
     // Bubble chart
-    var bubbleData = [{
+    let bubbleData = [{
       x: selectedSample.otu_ids,
       y: selectedSample.sample_values,
       text: selectedSample.otu_labels,
@@ -51,7 +51,7 @@ d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1
       }
     }];
 
-    var bubbleLayout = {
+    let bubbleLayout = {
       title: "OTU Bubble Chart",
       xaxis: { title: "OTU ID" },
       yaxis: { title: "Sample Values" }
@@ -60,10 +60,35 @@ d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
   }
 
+  //build metadata display
+  function buildMetadata(sample) {
+    // Filter metadata for the selected sample
+    let selectedMetadata = data.metadata.filter(function(d) {
+      return d.id == sample;
+    })[0];
 
+    // Select the demographic info panel
+    let demographicInfo = d3.select("#sample-metadata");
+
+    // Clear existing metadata
+    demographicInfo.html("");
+
+    // Display 
+    Object.entries(selectedMetadata).forEach(function([key, value]) {
+      demographicInfo.append("p").text(`${key}: ${value}`);
+    });
+  }
+
+  // Create the function for drop down option
+  function optionChanged(newSample) {
+    // Update charts and metadata when a new sample is selected
+    buildCharts(newSample);
+    buildMetadata(newSample);
+  }
 
   // Initialize the dashboard
   init();
 });
+
 
 
